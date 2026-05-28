025f51d255a12e09fd30af681f8f9c651ad68ba7
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-05-28 11:50:39 +0300

    Register employees resource in react-admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
-import { Admin } from "react-admin";
-import { Layout } from "./Layout";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-export const App = () => <Admin layout={Layout}></Admin>;
+import { EmployeeList } from "./employees/EmployeeList";
+import { EmployeeCreate } from "./employees/EmployeeCreate";
+import { EmployeeEdit } from "./employees/EmployeeEdit";
+import { EmployeeShow } from "./employees/EmployeeShow";
+
+const dataProvider = jsonServerProvider("http://localhost:3002");
+
+function App() {
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="employees"
+        list={EmployeeList}
+        create={EmployeeCreate}
+        edit={EmployeeEdit}
+        show={EmployeeShow}
+      />
+    </Admin>
+  );
+}
+
+export default App;
